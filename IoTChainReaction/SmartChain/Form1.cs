b47b7dd6f6ac96bc6dc6b9a803b65c518bf006bf
Amir Granot <digiclick@hotmail.com>
2017-01-24 23:02:20 +0200

    asd

---
+++ IoTChainReaction/SmartChain/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace SmartChain
+{
+    public partial class Form1 : Form
+    {
+        MqttClient mq;
+        public Form1()
+        {
+            InitializeComponent();
+            mq = new MqttClient():
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
