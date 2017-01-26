56c4379726d3e2457e61f0a27a30662a3989b75d
DCAG <digiclick@hotmail.com>
2017-01-26 19:36:41 +0200

    usb ports detection - need to change to detect arduino ports

--- IoTChainReaction/SmartChain/Form1.cs
+++ IoTChainReaction/SmartChain/Form1.cs
@@ -6,26 +6,96 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using uPLibrary.Networking.M2Mqtt;
+using System.IO.Ports;
+using System.Management;
 
 namespace SmartChain
 {
     public partial class Form1 : Form
     {
-        MqttClient client;
+        MqttClient m_WhiteCubeClient;
+        SerialPort m_ArduinoConnection;
+
         public Form1()
         {
             InitializeComponent();
-            client = new MqttClient("139.162.222.115", 80, false, null, MqttSslProtocols.None,null,null);
-            byte ConnResult = client.Connect(Guid.NewGuid().ToString().Split('-')[4].Substring(0, 10), "MATZI", "MATZI", true, 60);
+            ConnectArduino();
+            ConnectWhiteCube();
+        }
 
-            client.Subscribe(new string[] { "matzi/#" },new byte[] { 0 });
-            client.MqttMsgSubscribed += Client_MqttMsgSubscribed;
-            client.ToString();
+        private void ConnectArduino()
+        {
+            m_ArduinoConnection = new SerialPort();
+            m_ArduinoConnection.PortName = AutoDetectArduinoPort();
+        }
+        #region Arduino Port Auto Detection
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+
+        private void SubscribeToWMIInstances()
+        {
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+        }
+
+        private string AutoDetectArduinoPort()
+        {
+
+        }
+        #endregion
+        private void ConnectWhiteCube()
+        {
+            m_WhiteCubeClient = new MqttClient("139.162.222.115", 80, false, null, MqttSslProtocols.None, null, null);
+            m_WhiteCubeClient.Subscribe(new string[] { "matzi/#" }, new byte[] { 0 });
+            m_WhiteCubeClient.MqttMsgSubscribed += Client_MqttMsgSubscribed;
+            m_WhiteCubeClient.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            byte ConnResult = m_WhiteCubeClient.Connect(Guid.NewGuid().ToString().Split('-')[4].Substring(0, 10), "MATZI", "MATZI", true, 60);
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            SetValue(Encoding.UTF8.GetString(e.Message));
+        }
+
+        delegate void valueDelegate(string value);
+        private void SetValue(string value)
+        {
+            if(textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new valueDelegate(SetValue), value);
+            }
+            else
+            {
+                textBox1.Text += value;
+            }
         }
 
         private void Client_MqttMsgSubscribed(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgSubscribedEventArgs e)
         {
-            //textBox1.Text += Environment.NewLine + e.ToString();
+            SetValue(e.MessageId.ToString());
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
